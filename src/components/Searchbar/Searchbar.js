82c3dc3324836c7294b6c14181c5b23f41a02294
Tonya-Shyva <toshash69@gmail.com>
2022-12-13 08:43:58 +0200

    Update Searchbar.js
    used hooks

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 
 // бібліотека для повідомлень  $npm install --save react-toastify------------
 import { toast } from 'react-toastify';
@@ -14,51 +14,45 @@ import {
 } from './Searchbar.styled';
 import { ReactComponent as SearchIcon } from '../icons/search.svg';
 
-class SearchBar extends Component {
-  state = {
-    searchValue: '',
+function SearchBar({ onFormSubmit }) {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleValueChange = event => {
+    setSearchValue(event.currentTarget.value);
   };
 
-  handleValueChange = event => {
-    this.setState({
-      searchValue: event.currentTarget.value,
-    });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.searchValue.trim() === '') {
+    if (searchValue.trim() === '') {
       // alert('Please, enter search value!');
       toast.error('Please, enter search value!');
       return;
     }
 
-    this.props.onFormSubmit(this.state.searchValue);
-    this.setState({ searchValue: '' });
+    onFormSubmit(searchValue);
+    setSearchValue('');
   };
 
-  render() {
-    return (
-      <SearchBarHeader>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <IconButton type="submit" aria-label="search button">
-            <SearchIcon />
-          </IconButton>
+  return (
+    <SearchBarHeader>
+      <SearchForm onSubmit={handleSubmit}>
+        <IconButton type="submit" aria-label="search button">
+          <SearchIcon />
+        </IconButton>
 
-          <SearchBarInput
-            type="text"
-            name="searchValue"
-            value={this.state.searchValue}
-            autocomplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.handleValueChange}
-          />
-        </SearchForm>
-      </SearchBarHeader>
-    );
-  }
+        <SearchBarInput
+          type="text"
+          name="searchValue"
+          value={searchValue}
+          autocomplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleValueChange}
+        />
+      </SearchForm>
+    </SearchBarHeader>
+  );
 }
 
 export default SearchBar;
